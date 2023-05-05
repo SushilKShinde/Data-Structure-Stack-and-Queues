e2ce74866df2924276eb38f7b544dc50cfa88c87
Sushil <skshinde2028@gmail.com>
2023-05-05 14:16:46 +0530

    [Sushil] Adding stacks

---
+++ Data Structure - Stack & Queues/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure___Stack___Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the stack and queues");
+            Console.WriteLine("Choose the correct option from the below options ");
+            Console.WriteLine("1.Stack\n2.Queue");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    //Stacks stacks = new Stacks();
+
+                    Stacks.StackOperations();
+                    Stacks.StackFromArray();
+
+                    break;
+                default:
+                    Console.WriteLine("Choose Valid Option");
+                    break;
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Data Structure - Stack & Queues/Stacks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure___Stack___Queues
+{
+    public class Stacks
+    {
+        public static void StackOperations()
+        {
+            Stack<int> myStack = new Stack<int>();
+            myStack.Push(10);
+            myStack.Push(20);
+            myStack.Push(30);
+            myStack.Push(40);
+            Console.WriteLine("Stack elemenst are\n");
+            foreach(int element in myStack)
+                Console.Write(element+" ");
+        }
+
+        public static void StackFromArray()
+        {
+            Console.WriteLine("\n************************");
+            int[] arr = new int[] { 1,2,3,4 };
+            Stack<int> myStack = new Stack<int>(arr);
+            Console.WriteLine("Stack elemenst from array are\n");
+            foreach (int element in myStack)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+       
+       
+    }
+}
